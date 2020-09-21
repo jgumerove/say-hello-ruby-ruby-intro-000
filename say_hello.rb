f783981a30a2b6456d7297aeb9db6b96f76eb1f7
Josh Gumerove <josh.gumerove@gmail.com>
2020-09-21 22:46:46 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,6 @@
-def say_hello (name)
-  puts "Hello Josh Gumerove"
+def say_hello(name)
+puts "Hello Kent Beck"
 end
 
-def say_hello (name = "Ruby Programmer!")
+def say_hello(name="Ruby Programmer!")
   puts ("Hello #{name}!")
-end
-say_hello("Ruby Programmer")
-end
